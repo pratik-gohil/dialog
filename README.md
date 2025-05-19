f57100b11112239ce24ba727ee89a990827175d3
Pratik Gohil <pr.tikgohil@gmail.com>
2025-05-20 01:00:44 +0530

    chore: setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# IceWrap Dialog
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        IceWrap
+      </h1>
+    </main>
+  )
+}
+
+export default App
